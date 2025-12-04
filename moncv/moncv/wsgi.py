2ff2ef66f6a5c10d0b73f62684c6636c20af92b8
Hassan <hassan@example.com>
2025-12-04 03:10:01 +0100

    Fix wsgi.py: use moncv.settings_railway instead of moncv.settings_prod

--- moncv/moncv/wsgi.py
+++ moncv/moncv/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moncv.settings_prod')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moncv.settings_railway')
 
 application = get_wsgi_application()
 
